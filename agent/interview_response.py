e0414820c1b4d288791c90885d7f0ce5e7706458
Benson PENG <bensonpeng@qq.com>
2025-03-11 21:45:47 +0800

    update doc

--- agent/interview_response.py
+++ agent/interview_response.py
@@ -16,20 +16,7 @@ class Question(BaseModel):
     question_type: QuestionType = Field(description="The type of the question")
     knowledge_point: str = Field(description="The knowledge point of the question")
     answer: str = Field(description="The answer of the question")
-
-
-# class AnalyzeAnswerResponse(BaseModel):
-  
-#     is_answer: bool = Field(description="Whether the answer is a valid answer to the previous question")
-#     feedback: str = Field(description="The feedback of the answer to the previous question")
-#     is_correct: bool = Field(description="Whether the answer is correct to the previous question")
-#     answer_analysis: str = Field(description="The analysis of the answer to the previous question")
-#     answer_score: int = Field(description="The score of the answer to the previous question (0-5)")
-
-#     is_interview_over: bool = Field(description="Whether the interview is over")
-
-#     next_question: Question | None = Field(description="Generate a new question for the interview")
-
+    
 
 class Answer(BaseModel):
     is_valid: bool = Field(description="Whether the answer is a valid response")
